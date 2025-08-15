a91e7c7d92096035a9c59066087c55b91cb69948
prafullofcode <dahal.praphul268@gmail.com>
2025-08-15 20:20:52 +0545

    partial functionality

--- browser-extension-manager/js/load.js
+++ browser-extension-manager/js/load.js
@@ -2,7 +2,9 @@ function loadData() {
   fetch("data.json")
     .then((response) => response.json())
     .then((json) => {
+      console.log("data loading......");
       let container = document.getElementById("blocks-container");
+      removeData();
       json.extensions.forEach((extension) => {
         const active = extension.isActive ? "active" : " ";
         container.innerHTML += `
@@ -22,7 +24,7 @@ function loadData() {
                 </div>
                 <div class="block-bottom">
                   <div class="remove-button">Remove</div>
-                  <div class="extension-toggle ${active}" onclick="toggle(this);">
+                  <div class="extension-toggle ${active}" onclick="toggle(this); changeData(this)">
                     <div class="circle"></div>
                   </div>
                 </div>
@@ -32,101 +34,84 @@ function loadData() {
     });
 }
 
-// function changeData(element) {
-//   fetch("data.json")
-//     .then((response) => response.json())
-//     .then((json) => {
-//       let container = document.getElementById("blocks-container");
-//       const block = element.closest(".block");
-//       const h3 = block.querySelector("h3");
+function changeData(element) {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((json) => {
+      let container = document.getElementById("blocks-container");
+      const block = element.closest(".block");
+      const h3 = block.querySelector("h3");
 
-//       json.extensions.forEach((extension) => {
-//         if (extension.name == h3.textContent.trim()) {
-//           if (element.classList.contains("active")) {
-//             extension.isActive = true;
-//           } else {
-//             extension.isActive = false;
-//           }
-//           console.log(`${extension.name} is now ${extension.isActive} `);
-//         }
-//       });
-//     });
-// }
+      json.extensions.forEach((extension) => {
+        if (extension.name == h3.textContent.trim()) {
+          if (element.classList.contains("active")) {
+            extension.isActive = true;
+          } else {
+            extension.isActive = false;
+          }
+          console.log(`${extension.name} is now ${extension.isActive} `);
+        }
+      });
+    });
+}
 
-// function removeData() {
-//   let container = document.getElementById("blocks-container");
-//   container.innerHTML = " ";
-// }
+function removeData() {
+  let container = document.getElementById("blocks-container");
+  container.innerHTML = " ";
+}
 
-// function loadActive() {
-//   removeData();
-//   fetch("data.json")
-//     .then((response) => response.json())
-//     .then((json) => {
-//       let container = document.getElementById("blocks-container");
-//       json.extensions.forEach((extension) => {
-//         if (extension.isActive == true) {
-//           const active = extension.isActive ? "active" : " ";
-//           container.innerHTML += `
-//               <div class="block">
-//                 <div class="block-top">
-//                   <img
-//                     src="${extension.logo}"
-//                     alt="${extension.name} logo"
-//                     class="extension-logo"
-//                     height="60"
-//                     width="60"
-//                   />
-//                   <div class="block__text">
-//                     <h3 class = "extension-name">${extension.name}</h3>
-//                     <p>${extension.description}</p>
-//                   </div>
-//                 </div>
-//                 <div class="block-bottom">
-//                   <div class="remove-button">Remove</div>
-//                   <div class="extension-toggle ${active}" onclick="toggle(this);">
-//                     <div class="circle"></div>
-//                   </div>
-//                 </div>
-//               </div>
-//             `;
-//         }
-//       });
-//     });
-// }
+function loadActive() {
+  removeData();
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log("active data loading...");
+      let container = document.getElementById("blocks-container");
+      json.extensions.forEach((extension) => {
+        if (extension.isActive == true) {
+          changeInnerHtml(container, extension, "active");
+        }
+      });
+    });
+}
 
-// function loadInactive() {
-//   removeData();
-//   fetch("data.json")
-//     .then((response) => response.json())
-//     .then((json) => {
-//       let container = document.getElementById("blocks-container");
-//       json.extensions.forEach((extension) => {
-//         if (extension.isActive == false) {
-//           container.innerHTML += `
-//               <div class="block">
-//                 <div class="block-top">
-//                   <img
-//                     src="${extension.logo}"
-//                     alt="${extension.name} logo"
-//                     class="extension-logo"
-//                     height="60"
-//                     width="60"
-//                   />
-//                   <div class="block__text">
-//                     <h3 class = "extension-name">${extension.name}</h3>
-//                     <p>${extension.description}</p>
-//                   </div>
-//                 </div>
-//                 <div class="block-bottom">
-//                   <div class="remove-button">Remove</div>
-//                   <div class="extension-toggle" onclick="toggle(this);">
-//                     <div class="circle"></div>
-//                   </div>
-//                 </div>
-//               </div>
-//             `;
-//         }
-//       });
-//     });
-// }
+function loadInactive() {
+  removeData();
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log("inactive data loading...");
+      let container = document.getElementById("blocks-container");
+      json.extensions.forEach((extension) => {
+        if (extension.isActive == false) {
+          changeInnerHtml(container, extension, " ");
+        }
+      });
+    });
+}
+
+function changeInnerHtml(container, extension, mode) {
+  container.innerHTML += `
+              <div class="block">
+                <div class="block-top">
+                  <img
+                    src="${extension.logo}"
+                    alt="${extension.name} logo"
+                    class="extension-logo"
+                    height="60"
+                    width="60"
+                  />
+                  <div class="block__text">
+                    <h3 class = "extension-name">${extension.name}</h3>
+                    <p>${extension.description}</p>
+                  </div>
+                </div>
+                <div class="block-bottom">
+                  <div class="remove-button">Remove</div>
+                  <div class="extension-toggle ${mode}" onclick="toggle(this);">
+                    <div class="circle"></div>
+                  </div>
+                </div>
+              </div>
+            `;
+}
--- browser-extension-manager/js/script.js
+++ browser-extension-manager/js/script.js
@@ -1,5 +1,20 @@
 loadData();
 function toggle(element) {
   element.classList.toggle("active");
-  //   changeData(element);
 }
+
+let buttons = document.querySelectorAll(".button_hero");
+let activeButton = null;
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    button.classList.add("active");
+    activeButton = index;
+    console.log(activeButton);
+    if (activeButton == 0) loadData();
+    else if (activeButton == 1) loadActive();
+    else loadInactive();
+  });
+});
